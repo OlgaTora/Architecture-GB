a8e28273e18890696c283f9a05d7c21134d742c1
Olga Torres <bestlaly@mail.ru>
2023-10-12 16:01:11 +0300

    hw1 add main

--- src/lesson1/store/Program.java
+++ src/lesson1/store/Program.java
@@ -14,19 +14,48 @@ public class Program {
     public static void main(String[] args) {
         Texture texture1 = new Texture("texture 1");
         Texture texture2 = new Texture("texture 2");
-        Point3D points1 = new Point3D(1,1,1);
-        Point3D points2 = new Point3D(2,2,2);
-        Point3D points3 = new Point3D(3,3,3);
-        Poligon poligon = new Poligon();
-        Flash flash = new Flash(points1, "green", 100.0);
-        Camera camera = new Camera(points2);
-        Scene scene = new Scene();
+
+        Point3D point1 = new Point3D(1, 1, 1);
+        Point3D point2 = new Point3D(2, 2, 2);
+        Point3D point3 = new Point3D(3, 3, 3);
+
+        Collection<Point3D> points1 = new ArrayList<>();
+        points1.add(point1);
+        points1.add(point2);
+        Poligon poligon1 = new Poligon(points1);
+        Collection<Point3D> points2 = new ArrayList<>();
+        points1.add(point2);
+        points1.add(point3);
+        Poligon poligon2 = new Poligon(points2);
+        Collection<Poligon> poligons = new ArrayList<>();
+        poligons.add(poligon1);
+        poligons.add(poligon2);
+
+        PoligonalModel model1 = new PoligonalModel(poligons);
+        Collection<PoligonalModel> models = new ArrayList<>();
+        models.add(model1);
+
+        Flash flash1 = new Flash(point1, "green", 100.0);
+        Flash flash2 = new Flash(point2, "red", 50.0);
+        Collection<Flash> flashes = new ArrayList<>();
+        flashes.add(flash1);
+        flashes.add(flash2);
+
+        Camera camera1 = new Camera(point2);
+        Camera camera2 = new Camera(point3);
+        Collection<Camera> cameras = new ArrayList<>();
+        cameras.add(camera1);
+        cameras.add(camera2);
+
+        Scene scene1= new Scene(models, flashes, cameras);
+        Collection<Scene> scenes = new ArrayList<>();
+        scenes.add(scene1);
 
         CustomService1 observer1 = new CustomService1();
         CustomService2 observer2 = new CustomService2();
         CustomService3 observer3 = new CustomService3();
 
-        ModelStore modelStore = new ModelStore();
+        ModelStore modelStore = new ModelStore(models, flashes, cameras, scenes);
         modelStore.registerModelChanger(observer1);
         modelStore.registerModelChanger(observer2);
         modelStore.registerModelChanger(observer3);
