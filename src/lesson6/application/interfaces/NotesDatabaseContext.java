b4db306f06e8b1db50f0d4399de821ab0e42abba
Olga Torres <bestlaly@mail.ru>
2023-10-31 10:26:41 +0300

    lesson6 added

---
+++ src/lesson6/application/interfaces/NotesDatabaseContext.java
@@ -0,0 +1,13 @@
+package lesson6.application.interfaces;
+
+import ru.geekbrains.lesson6.domain.Note;
+
+import java.util.Collection;
+
+public interface NotesDatabaseContext {
+
+    Collection<Note> getAll();
+    boolean saveChanges();
+
+
+}
